8641545ca627eb3bdee11e211ef17ae64f9a89fc
Mradul999 <demorgans007@gmail.com>
2025-08-21 00:18:40 +0530

    commit

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -10,10 +10,10 @@ const Hero = () => {
       <div className="absolute inset-0 bg-orange-800 opacity-40" />
       <div className="relative max-w-7xl mx-auto px-4 py-20 grid md:grid-cols-2 gap-10 items-center">
         <div className="space-y-4">
-          <h1 className="text-3xl md:text-4xl font-extrabold leading-snug text-gray-900">
+          <h1 className="text-3xl md:text-4xl text-white font-extrabold leading-snug text-gray-900">
             United Auto parts BRINGS EXCLUSIVE USED & REMANUFACTURED AUTO PARTS FOR YOUR CAR
           </h1>
-          <p className="text-lg md:text-xl text-gray-800">
+          <p className="text-lg md:text-xl text-white font-semibold">
             Unleash Peak Performance and Efficiency with The Affordable Used Car Parts..
           </p>
           <img src={iconBan} alt="Icon" className="w-full h-30 mt-4" />
